02bc44b4a5c31f571a94d76a2657973db90eef5e
ShahSau <shahriarksaurov@gmail.com>
2020-11-03 21:56:22 +0200

    final

--- Login_page_with_authentication/app.js
+++ Login_page_with_authentication/app.js
@@ -1,33 +1,62 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
-//DB config
-const db = require("./config/keys").MongoURI;
+// Passport Config
+require('./config/passport')(passport);
 
-//connect to mongo
-mongoose.connect(db, {useNewUrlParser:true,useUnifiedTopology: true ,useFindAndModify: false})
-    .then(()=> console.log("Mongodb connected ...."))
-    .catch(err=> console.log(err));
+// DB Config
+const db = require('./config/keys').MongoURI;
 
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-
-
-//EJS
+// EJS
 app.use(expressLayouts);
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
-const PORT= process.env.PORT || 3000;
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-//Routes
-app.use("/", require("./routes/index"))
-app.use("/users", require("./routes/users"))
+// Connect flash
+app.use(flash());
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`)
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
